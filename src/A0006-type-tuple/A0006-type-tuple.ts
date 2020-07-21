5934d77f237b9bd8f3d5ba1d6714dfa304e59a11
MarceSanto <rocha.mer21@gmail.com>
2020-07-21 09:08:40 -0300

    tipo null e undefined

--- src/A0006-type-tuple/A0006-type-tuple.ts
+++ src/A0006-type-tuple/A0006-type-tuple.ts
@@ -4,7 +4,6 @@ const dadosClientes2: [number, string, string] = [1, 'Marcelo', 'Santos']
 const dadosClientes3: [number, string, string?] = [1, 'Marcelo']
 const dadosClientes4: [number, string, ...string[]] = [1, 'Marcelo', 'Santos']
 
-
 // dadosClientes1[0] = 100
 // dadosClientes1[1] = 'Gabriel'
 
---
+++ src/A0007-type-null-and-undefined/A0007-type-null-and-undefined.ts
@@ -0,0 +1,31 @@
+let x
+if (typeof x === 'undefined') x = 20
+
+console.log(x * 2)
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string
+  lastName?: string
+} {
+  return {
+    firstName,
+    lastName,
+  }
+}
+
+export function squareOf(x: any) {
+  if (typeof x === 'number') return x * x
+  return null
+}
+
+const squareOfTwoNumber = squareOf(2)
+const squareOfTwoString = squareOf('2')
+
+if (typeof squareOfTwoNumber === null) {
+  console.log('Conta inválida')
+} else {
+  console.log(squareOfTwoNumber * 100)
+}
