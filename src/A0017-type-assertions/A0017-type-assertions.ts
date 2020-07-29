f372c347ce9000472d6c1e6a52fcd9dd806ed910
MarceSanto <rocha.mer21@gmail.com>
2020-07-28 23:28:26 -0300

    Type assertion

--- src/A0017-type-assertions/A0017-type-assertions.ts
+++ src/A0017-type-assertions/A0017-type-assertions.ts
@@ -1,10 +1,7 @@
+/* Recomendado */
 // Condicional
 const body1 = document.querySelector('body')
 if (body1) body1.style.background = 'red'
-
-// Non-null assertion (!)
-const body2 = document.querySelector('body')!
-body2.style.background = 'red'
 
 // Type assertion
 const body3 = document.querySelector('body') as HTMLBodyElement
@@ -14,3 +11,12 @@ body3.style.background = 'red'
 const input = document.querySelector('.input') as HTMLInputElement
 input.value = 'Qualquer coisa'
 input.focus()
+
+/* Não Recomendado */
+
+// Type assertion
+const body4 = (document.querySelector('body') as unknown) as number
+
+// Non-null assertion (!)
+const body2 = document.querySelector('body')!
+body2.style.background = 'red'
