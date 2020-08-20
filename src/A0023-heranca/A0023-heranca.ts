00a85cb3e40848120792a7b89265d356748b7c6a
MarceSanto <rocha.mer21@gmail.com>
2020-08-19 22:13:08 -0300

    herança

---
+++ src/A0023-heranca/A0023-heranca.ts
@@ -0,0 +1,39 @@
+export class Pessoa {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    private idade: number,
+    protected cpf: string,
+  ) {}
+
+  getIdade(): number {
+    return this.idade
+  }
+
+  getCpf(): string {
+    return this.cpf
+  }
+
+  getNomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome
+  }
+}
+
+export class Aluno extends Pessoa {
+  getNomeCompleto(): string {
+    return 'Isso vem do Aluno: ' + this.nome + ' ' + this.sobrenome
+  }
+}
+export class Cliente extends Pessoa {
+  getNomeCompleto(): string {
+    return 'Isso vem do Cliente: ' + this.nome + ' ' + this.sobrenome
+  }
+}
+
+const pessoa = new Pessoa('Marcelo', 'Santos', 29, '128.128.128-28')
+const aluno = new Aluno('Marcelo', 'Santos', 29, '128.128.128-28')
+const cliente = new Cliente('Marcelo', 'Santos', 29, '128.128.128-28')
+
+console.log(aluno.getNomeCompleto())
+console.log(pessoa.getNomeCompleto())
+console.log(cliente.getNomeCompleto())
