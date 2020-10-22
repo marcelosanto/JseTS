ecbb3bbe470d73b05fa9a8066df335891904ad93
Marcelo Santos <rocha.mer21@gmail.com>
2020-10-21 23:08:43 -0400

    decoradores de parametro

--- src/A0057-decorator-param/A0057-decorator-param.ts
+++ src/A0057-decorator-param/A0057-decorator-param.ts
@@ -1,20 +1,42 @@
-renfunction decorator(
+function decorador(
   classPrototype: any,
-  nomeMetodo: string,
-  descriptor: PropertyDescriptor,
-): PropertyDescriptor {
+  nomeMetodo: string | symbol,
+  index: number,
+): any {
   console.log(classPrototype)
   console.log(nomeMetodo)
-  console.log(descriptor)
-  return {
-    value: function (...args: string[]) {
-      return args[0].toUpperCase()
-    },
+  console.log(index)
+}
+
+export class UmaPessoa {
+  nome: string
+  sobrenome: string
+  idade: number
+
+  constructor(@decorador nome: string, @decorador sobrenome: string,@decorador idade: number) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.idade = idade
+  }
+
+
+  metodo(@decorador msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`
+  }
+
+  get nomeCompleto(): string {
+    return `${this.nome} ${this.sobrenome}`
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g)
+    const primeiroNome = palavras.shift()
+    if (!primeiroNome) return
+    this.nome = primeiroNome
+    this.sobrenome = palavras.join(' ')
   }
 }
 
-
 const pessoa = new UmaPessoa('Alice', 'Santos', 8)
-pessoa.metodo = () => 'KKKkkkkkKKK'
 const metodo = pessoa.metodo('Oieee gatinha ')
 console.warn(metodo)
